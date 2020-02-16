f1e5130cf586e965be26d441832fb6511dabb367
curbAA <curbAAAA@gmail.com>
2020-02-16 15:14:44 -0300

    Responsive and working buttons page

--- public/modules/buttons/css/js/main.js
+++ public/modules/buttons/css/js/main.js
@@ -1,9 +1,9 @@
 function setButton(button, state){
     if (state == "pressed"){
         
-        button.style.marginBottom = "0";
-        button.style.marginRight = "0";
-        button.style.marginRight = "5";
+        button.style.marginBottom = "0px";
+        button.style.marginRight = "0px";
+        button.style.marginLeft = "2vw";
 
         button.style.background = "#dba16a";
         button.style.color = "#967252";
@@ -11,13 +11,13 @@ function setButton(button, state){
         button.style.filter = "none";
     } else if (state == "released"){
 
-        button.style.marginBottom = "5";
-        button.style.marginRight = "5";
-        button.style.marginRight = "0";
+        button.style.marginBottom = "2vw";
+        button.style.marginRight = "2vw";
+        button.style.marginLeft = "0px";
 
         button.style.background = "#c5c5c5";
         button.style.color = "#7e7e7e";
         
-        button.style.filter = "drop-shadow(5px 5px #7e7e7e)";
+        button.style.filter = "drop-shadow(2vw 2vw #7e7e7e)";
     }
 }
