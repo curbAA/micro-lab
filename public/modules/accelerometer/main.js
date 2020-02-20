9c7f142002693bc0ec204491002bddd76fff3c6e
curbAA <curbAAAA@gmail.com>
2020-02-20 06:10:29 -0300

    Improved charts look and implemented function for plotting live data in them

--- public/modules/accelerometer/main.js
+++ public/modules/accelerometer/main.js
@@ -18,6 +18,13 @@
     Chart.defaults.global.defaultFontSize = 10;
 
     var options = {
+
+        legend:{
+            display:false,
+            labels:{
+            }
+        },
+
         scales:{
 
             yAxes: [{
@@ -33,8 +40,6 @@
                     suggestedMax: 2000,
                     suggestedMin: -2000,
                     maxTicksLimit: 5,
-
-                    fontStyle:"bold",
                 },
             }],
 
@@ -45,7 +50,6 @@
                 },
                 ticks:{
                     beginAtZero: false,
-                    maxTicksLimit: 10,
                 }
             }]
         }
@@ -56,6 +60,7 @@
         labels:["0", "1", "2", "3", "4", "5"],
         datasets:{
             data:[500,1000,-1500,2000,-1000,-500],
+            borderWidth:2,
         },
     };
 
@@ -69,12 +74,11 @@
         var x_chart = new Chart(x_ctx, {
             type: default_chart_type,
             data: {
-                labels:default_chart_data.labels,
                 datasets: [{
                     label: "X",
                     backgroundColor: "rgba(255, 50, 100, 0.2)",
                     borderColor: "rgba(255, 50, 100, 0.7)",
-                    data: default_chart_data.datasets.data,
+                    borderWidth: default_chart_data.datasets.borderWidth,
                 }]
             },
             options: options
@@ -89,12 +93,11 @@
         var y_chart = new Chart(y_ctx, {
             type: default_chart_type,
             data: {
-                labels:default_chart_data.labels,
                 datasets: [{
                     label: "Y",
                     backgroundColor: "rgba(100, 50, 255, 0.2)",
                     borderColor: "rgba(100, 50, 255, 0.7)",
-                    data: default_chart_data.datasets.data,
+                    borderWidth: default_chart_data.datasets.borderWidth,
                 }]
             },
             options: options
@@ -109,16 +112,53 @@
         var z_chart = new Chart(z_ctx, {
             type: default_chart_type,
             data: {
-                labels:default_chart_data.labels,
                 datasets: [{
                     label: "Z",
                     backgroundColor: "rgba(50, 255, 100, 0.2)",
                     borderColor: "rgba(50, 255, 100, 0.7)",
-                    data: default_chart_data.datasets.data,
+                    borderWidth: default_chart_data.datasets.borderWidth,
                 }]
             },
             options: options
         });
+
+    //
+    // ──────────────────────────────────────── ANCHOR ADD DATA TO CHART FUNCTION ─────
+    //
+
+        function chartPlotData(chart, value, index, limit){
+            console.log(chart.data.datasets[0].data.length);
+
+            if(chart.data.datasets[0].data.length < limit){
+
+                chart.data.datasets[0].data.push(value);
+                chart.data.labels.push(index);
+            } else {
+
+                chart.data.datasets[0].data.shift();
+                chart.data.datasets[0].data.push(value);
+                
+                chart.data.labels.shift();
+                chart.data.labels.push(index);
+            }
+
+            chart.update();
+        }
+
+        chartPlotData(x_chart, 2000, "0", 4);
+        chartPlotData(x_chart, 100, "1", 4);
+        chartPlotData(x_chart, -1500, "2", 4);
+        chartPlotData(x_chart, -1500, "3", 4);
+
+        chartPlotData(y_chart, 2000, "0", 4);
+        chartPlotData(y_chart, 100, "1", 4);
+        chartPlotData(y_chart, -1500, "2", 4);
+        chartPlotData(y_chart, -1500, "3", 4);
+
+        chartPlotData(z_chart, 2000, "0", 4);
+        chartPlotData(z_chart, 100, "1", 4);
+        chartPlotData(z_chart, -1500, "2", 4);
+        chartPlotData(z_chart, -1500, "3", 4);
         
 
         
